2c038a55174d2bb4af15cd683013644895a148dd
Ibrahim Bandaogo <ibandaogo@gmail.com>
2021-01-03 23:36:20 +0000

    Topic 11 - I've completed all my labs!

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -21,16 +21,70 @@ router.put('/:id',  (req, res) => {
     .then(user => res.json(200, user));
 });
 
+// Register OR authenticate a user
+router.post('/', async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({
+      success: false,
+      msg: 'Please pass username and password.',
+    });
+  }
+  if (req.query.action === 'register') {
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+    if (regex.test(req.body.password)) {
+      await User.create(req.body).catch(next);
+      res.status(201).json({
+        code: 201,
+        msg: 'Successful created new user.',
+      });
+    }
+    else {
+      res.status(400).json({
+        code: 400,
+        msg: "System fail to Create User. Check your Passwords characters not less than 5 characters long and must contain at least 1 number and 1 letter."
+      })
+    }
+  
+  } else {
+    const user = await User.findByUserName(req.body.username).catch(next);
+      if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          const token = jwt.sign(user.username, process.env.SECRET);
+          // return the information including token as JSON
+          res.status(200).json({
+            success: true,
+            token: 'BEARER ' + token,
+          });
+        } else {
+          res.status(401).json({
+            code: 401,
+            msg: 'Authentication failed. Wrong password.'
+          });
+        }
+      });
+    }
+});
 
 //Add a favourite. No Error Handling Yet. Can add duplicates too!
 router.post('/:userName/favourites', async (req, res, next) => {
+  
+  try{
   const newFavourite = req.body.id;
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
+  if (user.favourites.includes(movie.id)){
   await user.favourites.push(movie._id);
   await user.save(); 
-  res.status(201).json(user); 
+  res.status(201).json(user)
+  }
+  }
+  catch(err){
+    next(err)
+
+  } 
 });
 
 router.get('/:userName/favourites', (req, res, next) => {
