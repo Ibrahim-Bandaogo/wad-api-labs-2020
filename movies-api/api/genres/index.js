291921fa5155b503915a840eaf0f7b7a456d1601
Ibrahim Bandaogo <ibandaogo@gmail.com>
2021-01-24 16:22:43 +0000

    Genre by id

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -10,5 +10,10 @@ const router = express.Router();
 router.get('/', (req, res,next) => {
   getGenres().then(movies => res.status(200).send(movies));
 });
+//////
+router.get('/:id', (req, res,next) => {
+  getGenres().then(genres => res.status(200).send(genres.filter(genre => genre.id == req.params.id)));
+});
+/////
 
 export default router;
